ad25982d03eb3f5634222a68f425b8e841bfa239
Joshua Seitz <SeitzJoshua7@gmail.com>
2018-04-21 22:11:41 -0400

    testing

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'askit-api',
+  :user_name => 'askit',
   :password => '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',
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
