5ec50795bc5e0e14f81a15750a39f6f6a2f2237c
Joshua Seitz <SeitzJoshua7@gmail.com>
2018-04-21 22:32:01 -0400

    fix bugs again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { :host => 'askit-team2.herokuapp.com', :protocol => 'https'}
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
