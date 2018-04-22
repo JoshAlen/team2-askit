bff19ea7612ae8fd22960b728b5826326138b2b6
Joshua Seitz <SeitzJoshua7@gmail.com>
2018-04-22 14:56:35 -0400

    removed sendgrid email and password

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'app91212498@heroku.com',
-  :password => 'xygadhkt7746',
-  :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
